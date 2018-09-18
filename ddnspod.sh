658bfcbf677b6bb6dc6b8e5622c8f5280dd1c8dd
YiWu <eric@jiangda.info>
2018-09-19 04:13:05 +0800

    modified:   ddnspod.sh

--- ddnspod.sh
+++ ddnspod.sh
@@ -8,32 +8,54 @@ login_token='0123456789abcdef'
 domain='www.domain.com'
 ###   end
 
-agent="ddnspod/0.4(eric@jiangda.info)"
+    current_ip=`curl -s ip.cn | grep -Eo "([0-9]{1,3}[\.]){3}[0-9]{1,3}"`
+echo "ddns start"
+agent="ddnspod/0.6(eric@jiangda.info)"
 postdata="login_token=${user_id},${login_token}&format=json&domain=${domain#*.}&sub_domain=${domain%%.*}"
-returnjson=`curl -s -A ${agent} -X POST https://dnsapi.cn/Record.List -d "${postdata}"`
-returncode=${returnjson#*code\":\"}
-if [ ${returncode%%\"*} != '1' ]
-then
+
+while :
+do
+    sleep 3m;
+    returnjson=`curl -s -A ${agent} -X POST https://dnsapi.cn/Record.List -d "${postdata}"`
+    returncode=${returnjson#*code\":\"}
+    if [ ${returncode%%\"*} == '1' ]
+    then
+        echo "${returnjson}"
+        break
+    fi
     echo "Failed to get the record for ${domain}"
-    exit
-fi
+done
+
 returnjson=${returnjson#*records\":[}
 record_id=${returnjson#*id\":\"}
 record_line_id=${returnjson#*line_id\":\"}
 value=${returnjson#*value\":\"}
+echo "${value}"
 last_ip=${value%%\"*}
 postdata="${postdata}&record_line_id=${record_line_id%%\"*}&record_id=${record_id%%\"*}"
+
 while :
 do
-    current_ip=`curl -s ip.cn | grep -Eo "([0-9]{1,3}[\.]){3}[0-9]{1,3}"`
+    # get WAN IP from ip.cn
+    # current_ip=`curl -s ip.cn | grep -Eo "([0-9]{1,3}[\.]){3}[0-9]{1,3}"`
+
+    # get WAN IP from openwrt
+    #current_ip=`ip add show pppoe-wan | grep -Eo "([0-9]{1,3}[\.]){3}[0-9]{1,3}"`
+    current_ip=`ip add show pppoe-wan | grep inet | cut -d " "  -f6`
+    date
+    echo "WAN IP is ${current_ip}"
+    echo "DNS IP is ${last_ip}"
     if [ ${current_ip} != ${last_ip} ]
     then
         returnjson=`curl -s -A ${agent} -X POST https://dnsapi.cn/Record.Ddns -d "${postdata}"`
         returncode=${returnjson#*code\":\"}
+        echo "update code is ${returncode}"
         if [ ${returncode%%\"*} = '1' ]
         then
             last_ip=${current_ip}
+            echo "Update successful"
         fi
     fi
-    sleep 5m
+    echo ""
+    sleep 3m
 done
